b96ed3eeb4929e79064ba0ffd2ba4ecd8759dbd9
michaelBussy <michael552@interia.pl>
2018-09-02 01:18:40 +0200

    v. 1.0.5

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bartek
+ * Date: 02.09.18
+ * Time: 01:06
+ */
+
+//App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+
+//URL Root
+define('URL_ROOT', 'http://localhost/back/coursemvc1');
+
+//Site Name
+define('SITE_NAME', 'MichaelMVC');
